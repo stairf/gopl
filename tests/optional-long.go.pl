4aa01dbb870656e5413e0cdc5ce688f3d8f57811
Stefan Reif <stefan.reif@fau.de>
2014-04-14 17:03:00 +0200

    Add testcase optional-long
    This TC tests long options with optional values.

---
+++ tests/optional-long.go.pl
@@ -0,0 +1,7 @@
+
+@options = (
+	{ long => "foo,bar", type => "int", optional => "yes", default => "-1" },
+	{ long => "goo", type => "string", optional => "yes" },
+	{ long => "hoo", type => "string" },
+);
+
