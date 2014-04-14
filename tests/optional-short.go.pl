5f72b624f82d334f00a03535b9fb036c54814c2a
Stefan Reif <stefan.reif@fau.de>
2014-04-14 17:02:26 +0200

    Add testcase optional-short
    This TC tests short options with optional values.

---
+++ tests/optional-short.go.pl
@@ -0,0 +1,7 @@
+
+@options = (
+	{ short => 'x', type => 'int', optional => "yes", default => "-1", },
+	{ short => 'y', type => 'string', optional => "yes", },
+	{ short => 'z', type => 'string' },
+);
+
